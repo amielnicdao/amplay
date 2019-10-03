f9d7d12cd239daf8fb5fb0e4e3dbce68eaec62cd
anicdao <amielnicdao@gmail.com>
2019-10-02 20:59:13 -0400

    added ids in the html file, added variables for wins, losses, letters, guesses left, added document.onkeyup, started on logic for keeping track of score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,24 @@
 // create an array that lists all the letter (??)
 // create variables that hold wins, losses, # of guesses left, user guesses. I will use .textContent. 
 // 
+
+var letters = "";
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 3;
+
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+    var computerGuess = letters[Math.floor(Math.random() * letters.Length)];
+
+    if (userGuess === computerGuess) {
+        wins++;
+    } else if (userGuess !== computerGuess && guessesLeft === 0) {
+        losses++;
+    } else {}
+
+
+
+};
