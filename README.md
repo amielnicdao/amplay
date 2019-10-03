86e99549c10f2faefc5fae63b23b258f4cb1b889
anicdao <amielnicdao@gmail.com>
2019-10-03 18:31:43 -0400

    put computerGuess outside of function and created a new one so it restarts only when user loses or wins.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # amplay
 The Psychic Game
+
+Description: A game coded in vanilla Javascript. The computer chooses a random letter and the user must guess what letter it is. User
+gets 5 tries before losing. 
+
+Instructions:
+
+1. Press a letter.
+2. If you guess correctly, you will get 1 point under win and game will restart. 
+3. If you guess incorrectly, your guesses left will decrease by 1 and you will see your incorrect guesses under "your guesses."
+4. If your "Guesses Left" reaches 0, you will get a point under losses and game will restart.
+
+Good luck!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,37 +8,51 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 3;
 var guesses = [];
+var computerGuess
 
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesleft-text");
 var lettersGuessedText = document.getElementById("lettersguessed-text");
 
+
+function startGame() {
+  computerGuess = letters[Math.floor(Math.random() * letters.length)];
+}
+
 document.onkeyup = function (event) {
   
-    userGuess = event.key;
-    var computerGuess = letters[Math.floor(Math.random() * letters.Length)];
-  
+    var userGuess = event.key;
+    
+    console.log(computerGuess)
+    console.log(userGuess)
+   
     if (userGuess === computerGuess) {
       wins++;
       guessesLeft = 3;
-    
+      startGame()
     } if (userGuess !== computerGuess) {
       guessesLeft--;
-
     } if (guessesLeft === 0) {
       losses++;
       guessesLeft = 3;
+      startGame()
     }
+
+
 
 winsText.textContent = "Wins: " + wins;
 lossesText.textContent = "Losses: " + losses;
 guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
 lettersGuessedText.textContent = "Your guesses: " + userGuess;
 
-// userGuess.append(guesses);
+ guesses.push(userGuess);
+ console.log(guesses)
 
 //figure out why there's  no wins
 //why isn't the user input continously recording
 //do i create a for loop?
+
 };
+
+startGame()
