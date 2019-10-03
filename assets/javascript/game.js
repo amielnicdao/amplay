7e8f601841eee1f4517580b2e4e1108b4176aa4f
anicdao <amielnicdao@gmail.com>
2019-10-02 22:53:21 -0400

    added .textContent, document.getElement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,17 @@
 // create variables that hold wins, losses, # of guesses left, user guesses. I will use .textContent. 
 // 
 
-var letters = "";
+var letters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 3;
+var guesses = [];
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessesleft-text");
+var lettersGuessedText = document.getElementById("lettersguessed-text");
 
 document.onkeyup = function(event) {
 
@@ -18,7 +24,18 @@ document.onkeyup = function(event) {
     } else if (userGuess !== computerGuess && guessesLeft === 0) {
         losses++;
     } else {}
+        guessesLeft--;
 
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+lettersGuessedText.textContent = "Your guesses: " + userGuess;
 
+userGuess.append(guesses);
 
+//figure out why there's  no wins
+//why isn't the user input continously recording
+//the guesses left is not restarting
+//how to restart after a win or a loss?
+//do i create a for loop?
 };
