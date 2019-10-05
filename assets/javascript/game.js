aea2445b1632ce55337ea4e5395433f0b4c98abf
anicdao <amielnicdao@gmail.com>
2019-10-05 10:12:10 -0400

    fixed html and js so letters guessed are alongside Your Guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,11 @@
-// create an array that lists all the letter (??)
-// create variables that hold wins, losses, # of guesses left, user guesses. I will use .textContent. 
-// 
 
 var letters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 3;
+var guessesLeft = 15;
 var guesses = [];
-var computerGuess
+
 
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
@@ -22,20 +19,17 @@ function startGame() {
 document.onkeyup = function (event) {
   
     var userGuess = event.key;
-    
-    console.log(computerGuess)
-    console.log(userGuess)
    
     if (userGuess === computerGuess) {
       wins++;
-      guessesLeft = 3;
+      guessesLeft = 15;
       guesses = [];
       startGame()
     } if (userGuess !== computerGuess) {
       guessesLeft--;
     } if (guessesLeft === 0) {
       losses++;
-      guessesLeft = 3;
+      guessesLeft = 15;
       startGame()
       guesses = [];
     }
@@ -43,16 +37,11 @@ document.onkeyup = function (event) {
 winsText.textContent = "Wins: " + wins;
 lossesText.textContent = "Losses: " + losses;
 guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
-lettersGuessedText.textContent = "Your guesses: " + userGuess;
+lettersGuessedText.textContent = "Your guesses: ";
+var newguesses = guesses.join(",")
 
 guesses.push(userGuess);
-document.getElementById("lettersguessed-text").innerHTML = guesses;
-console.log(guesses)
-
-
-//why does the text disappear?
-
-
+document.getElementById("allguesses").innerHTML = newguesses;
 };
 
 startGame()
